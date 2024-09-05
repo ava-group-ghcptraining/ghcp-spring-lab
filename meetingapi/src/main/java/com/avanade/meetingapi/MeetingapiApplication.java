f4716e6c0bfd00b831c6f11bad2c2635359df9a5
skogkatt <anas.sammane@gmail.com>
2024-09-05 14:53:34 +0200

    first sample for spring project

---
+++ meetingapi/src/main/java/com/avanade/meetingapi/MeetingapiApplication.java
@@ -0,0 +1,13 @@
+package com.avanade.meetingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingapiApplication.class, args);
+	}
+
+}
---
+++ meetingapi/src/test/java/com/avanade/meetingapi/MeetingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avanade.meetingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
